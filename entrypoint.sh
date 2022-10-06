ace360ed9a0fdc601b4393a6f498867d572cec9e
shanedie <115125906+shanedie@users.noreply.github.com>
2022-10-05 18:56:54 -0700

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#Config xray
+
+rm -rf /etc/xray/config.json
+cat << EOF > /etc/xray/config.json
+{
+  "inbounds": [
+    {
+      "port": $PORT,
+      "protocol": "$PROTOCOL",
+      "settings": {
+        "method": "2022-blake3-aes-128-gcm",
+		"password": "PASSWORD",
+		"network": "tcp,udp"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+#run xray
+
+xray -c /etc/xray/config.json
